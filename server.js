251aa04000a31b8a402cd2a6bf3f768a09329b1f
Adil Waqar <adwiq78@gmail.com>
2020-05-06 01:05:33 +0500

    bug fix

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('API running'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
